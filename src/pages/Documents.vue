4792311d0059a7f48095154aa47f58287c3eefa0
Nikita Antonov <nikita45454@gmail.com>
2025-09-09 09:38:18 +0300

    добавляет политику конфиденциальности в документы

--- src/pages/Documents.vue
+++ src/pages/Documents.vue
@@ -4,10 +4,17 @@
       <div class="2xl:tw-flex 2xl:tw-justify-between">
         <h1 class="h1 tw-mb-20">документы</h1>
         <div class="tw-text-md tw-text-secondary tw-leading-120 tw-max-w-[248px] md:tw-max-w-[600px] md:tw-font-extrabold md:tw-text-lg 2xl:tw-text-xl 2xl:tw-max-w-[862px] 2xl:tw-leading-100 2xl:tw-pt-30">
-          Разрешительная документация на&nbsp;сайте
-          <a class="tw-text-[#3AB2ED] tw-underline md:tw-no-underline" :href="$store.state.domrf" target="_blank">
-            наш.дом.рф
-          </a>
+          <p>
+            Разрешительная документация на&nbsp;сайте
+            <a class="tw-text-[#3AB2ED] tw-underline md:tw-no-underline" :href="$store.state.domrf" target="_blank">
+              наш.дом.рф
+            </a>
+          </p>
+          <div class="tw-mt-24">
+            <a class="tw-text-[#3AB2ED] tw-underline md:tw-no-underline tw-text-md" href="/policy.pdf" target="_blank">
+              Политика конфиденциальности
+            </a>
+          </div>
         </div>
       </div>
     </div>
