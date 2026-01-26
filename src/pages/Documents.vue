6a54ee209b145a873c8909a156ca4f4566ecd363
Nikita Antonov <nikita45454@gmail.com>
2026-01-27 02:02:48 +0300

    обновляет документы

--- src/pages/Documents.vue
+++ src/pages/Documents.vue
@@ -3,16 +3,19 @@
     <div class="wrapper tw-relative decor-rock">
       <div class="2xl:tw-flex 2xl:tw-justify-between">
         <h1 class="h1 tw-mb-20">документы</h1>
-        <div class="tw-text-md tw-text-secondary tw-leading-120 tw-max-w-[248px] md:tw-max-w-[600px] md:tw-font-extrabold md:tw-text-lg 2xl:tw-text-xl 2xl:tw-max-w-[862px] 2xl:tw-leading-100 2xl:tw-pt-30">
+        <div class="tw-text-md tw-text-secondary tw-leading-120 tw-pb-32 tw-max-w-[248px] md:tw-max-w-[600px] md:tw-font-extrabold md:tw-text-lg 2xl:tw-text-xl 2xl:tw-max-w-[862px] 2xl:tw-leading-100 2xl:tw-pt-30">
           <p>
             Разрешительная документация на&nbsp;сайте
             <a class="tw-text-[#3AB2ED] tw-underline md:tw-no-underline" :href="$store.state.domrf" target="_blank">
               наш.дом.рф
             </a>
           </p>
-          <div class="tw-mt-24">
-            <a class="tw-text-[#3AB2ED] tw-underline md:tw-no-underline tw-text-md" href="/policy.pdf" target="_blank">
+          <div class="tw-mt-24 tw-space-y-12">
+            <a class="tw-text-[#3AB2ED] tw-underline md:tw-no-underline tw-text-base tw-block tw-w-fit" href="/policy.pdf" target="_blank">
               Политика конфиденциальности
+            </a>
+            <a class="tw-text-[#3AB2ED] tw-underline md:tw-no-underline tw-text-base tw-block tw-w-fit" href="/sogl.pdf" target="_blank">
+              Cогласие на обработку персональных данных
             </a>
           </div>
         </div>
